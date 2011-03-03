6ee1687655543c61dcb96d70788ea64dd432db3a
G5 Engineering <g5engineering@G5-Admins-iMac.local>
2011-03-02 17:42:36 -0800

    Code and view cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @stories = Story.all.group_by(&:current_state)
     @build_feed = Feedzirra::Feed.fetch_and_parse("http://g5search:g5rocks@sancho.g5search.com/projects/core-development.rss")
-    @activity_feed = Feedzirra::Feed.fetch_and_parse("https://www.pivotaltracker.com/user_activities/6719535ac5ba30ac90329524c0d0aa43")
   end
 
   def rebuild
@@ -14,5 +13,9 @@ class WelcomeController < ApplicationController
       persistent_story.save
     end
     redirect_to root_url
+  end
+
+  def chatter
+    @activity_feed = Feedzirra::Feed.fetch_and_parse("https://www.pivotaltracker.com/user_activities/6719535ac5ba30ac90329524c0d0aa43")
   end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Base
   attr_accessible :url, :project_id, :name, :description, :accepted_at,
-                  :story_type, :estiamte, :current_state, :requested_by,
+                  :story_type, :estimate, :current_state, :requested_by,
                   :owned_by, :labels
   def first_label
     labels? ? labels.split(',').first : 'none'
