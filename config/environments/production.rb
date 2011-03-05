cfe6b1ed8d78c3d708e0e2dc77c0828a5af4bf17
Chris Kraybill <ckraybill@gmail.com>
2011-03-05 12:00:04 -0800

    Heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ PivotalDashboard::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # For faster heroku slug compilation
+  config.serve_static_assets = true
 end
